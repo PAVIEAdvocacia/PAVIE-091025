7ddd234480f656bf9fc90ea6bcdffa6f789f9751
fabiopavie <fabiopavie@pavieadvogado.com>
2025-10-17 00:01:20 -0300

    fgfgfg
    fgfgf

--- form.turnstile.js
+++ form.turnstile.js
@@ -4,3 +4,11 @@ window.onTurnstileSuccess = (t) => {
   document.dispatchEvent(new CustomEvent("cf-token", { detail: token }));
 };
 export const getCfToken = () => token;
+
+
+// Exposição global opcional (bridge) para integração com form.submit.js
+try {
+  if (typeof window !== "undefined" && typeof window.getCfToken !== "function") {
+    window.getCfToken = getCfToken;
+  }
+} catch (_e) {}
