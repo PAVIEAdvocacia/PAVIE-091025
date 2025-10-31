ec9829dd65351cbfa0bcfb9fc6d0e9b40291e2cc
fabiopavie <fabiopavie@pavieadvogado.com>
2025-10-31 11:09:04 -0300

    fgfg
    dfgfg

--- blog/functions/auth/github.ts
+++ blog/functions/auth/github.ts
@@ -5,11 +5,17 @@ export async function onRequest(context) {
   const client_id = context.env.GITHUB_CLIENT_ID;
   const client_secret = context.env.GITHUB_CLIENT_SECRET;
 
-  const tokenRequest = await fetch(`https://github.com/login/oauth/access_token`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json", "Accept": "application/json" },
-    body: JSON.stringify({ client_id, client_secret, code })
-  });
+  const tokenRequest = await fetch(
+    `https://github.com/login/oauth/access_token`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({ client_id, client_secret, code })
+    }
+  );
 
   const data = await tokenRequest.json();
   return Response.json(data);
