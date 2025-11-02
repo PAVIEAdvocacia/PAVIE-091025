ed6a0b81d3140d5a88c541f06a4886b15a5ccd0f
fabiopavie <fabiopavie@pavieadvogado.com>
2025-11-02 13:10:35 -0300

    yyu
    yy

--- blog/src/pages/blog/admin/config.yml.ts
+++
@@ -1,9 +0,0 @@
-// Exemplo: se seu config ficou em <raiz do repo>/blog/admin/config.yml
-// ajuste o caminho relativo conforme a sua árvore:
-import raw from '../../../../blog/admin/config.yml?raw';
-
-export const GET = async () => {
-  return new Response(raw, {
-    headers: { 'Content-Type': 'text/yaml; charset=utf-8' },
-  });
-};
