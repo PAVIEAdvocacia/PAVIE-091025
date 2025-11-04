c604218363d849ccb186cc047b018bff8368ebc3
fabiopavie <fabiopavie@pavieadvogado.com>
2025-11-04 12:04:24 -0300

    ghgh
    ghg

--- functions/api/ping.ts
+++ functions/api/ping.ts
@@ -1,3 +1,7 @@
-export function onRequest() {
-  return new Response("pong-js", { headers: { "content-type": "text/plain" } });
-}
+export const onRequestGet: PagesFunction = async ({ request }) => {
+  return new Response(JSON.stringify({
+    ok: true,
+    url: request.url,
+    note: "Se você está vendo este JSON, /api/* está chegando às Functions."
+  }), { headers: { 'Content-Type': 'application/json' }});
+};
