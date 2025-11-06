c4c86aa4ef429fbe14d5fe52e8dc7f42cc0e2b89
fabiopavie <fabiopavie@pavieadvogado.com>
2025-11-06 14:12:20 -0300

    fgf
    gfg

--- functions/api/ping.ts
+++
@@ -1,7 +0,0 @@
-export const onRequestGet: PagesFunction = async ({ request }) => {
-  return new Response(JSON.stringify({
-    ok: true,
-    url: request.url,
-    note: "Se você está vendo este JSON, /api/* está chegando às Functions."
-  }), { headers: { 'Content-Type': 'application/json' }});
-};
